2e34c6d4c322d7d5869648087f2ff928af071889
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-10 12:30:49 +0800

    Load final command center centering fix

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import './force-ui.css';
 import './response-list-fix.css';
 import './major-layout-fix.css';
 import './command-center-layout-fix.css';
+import './command-center-final-fix.css';
 import CommandCenterEnhancements from './components/CommandCenterEnhancements';
 
 export const metadata = {
